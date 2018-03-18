8d1d527661a8c700d91d5b3db2c450c33cf8dc71
CChristiansen23 <christiansenconner@gmail.com>
2018-03-18 14:55:47 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <!-- Part 1 5 Points-->
+                    <!-- Create a Signup Form where you retrieve the following Information -->
+                    <!-- Full Name (First Name + Last Name) -->
+                    <!-- Mail -->
+                    <!-- Password -->
+                    <!-- Store Data? Yes/No -->
+                    <div class="form-group">
+                        <label for="name">First & Last Name</label>
+                        <input type="text" id="name" class="form-control" v-model="userData.name">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" id="email" class="form-control" v-model="userData.email">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" class="form-control" v-model="userData.password">
+                    </div>
+
+                    <div>
+                        <label for="password">Store Data?</label>
+
+                        <app-OnOff v-model="dataSwitch"></app-OnOff>
+                    </div>
+
+                    <div>
+                        <button class="btn btn-primary" @click.prevent="submitted">
+                            Submit!
+                        </button>
+                    </div>
+
+
+                    <!-- Part 2 5 Points -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+
+                    <!-- Part 3 5 Points -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>First & Last Name: {{userData.name}}</p>
+                        <p>Full Name: {{userData.name}}</p>
+                        <p>Mail: {{userData.email}}</p>
+                        <p>Password: {{userData.password}}</p>
+                        <p>Store in Database?: {{dataSwitch}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import OnOff from './OnOff.vue'
+    export default {
+        data(){
+            return {
+                userData: {
+                    name: '',
+                    email: '',
+                    password: ''
+                },
+                dataSwitch: true,
+                show: false
+            }
+        },
+        methods: {
+            submitted() {
+                this.show = true
+            }
+        },
+        components: {
+            appOnOff : OnOff
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/OnOff.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div id="on" @click="switched(true)" :class="{active: value}">Yes</div>
+
+        <div id="off" @click="switched(false)" :class="{active: !value}">No</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['value'],
+        methods: {
+            switched(isOn){
+                this.$emit('input', isOn)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #on, #off {
+        cursor: pointer;
+    }
+
+</style>
